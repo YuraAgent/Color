7ef4f020b916925c37a04314509518a254bdd022
YuraAgent <136337353+YuraAgent@users.noreply.github.com>
2026-03-11 12:18:06 +0300

    Add files via upload

---
+++ color.h
@@ -0,0 +1,13 @@
+#ifndef _COLOR_
+#define _COLOR_
+
+void ForegroundColor(int textColor)
+{
+    std::cout << "\033[" << textColor << "m";
+}
+void ClearColor()
+{
+    std::cout << "\033[0m";
+}
+
+#endif
